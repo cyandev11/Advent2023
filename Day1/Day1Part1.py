83f4a37f76ddb7f9e556e6dfdea4b7c22c0ea429
RyanLiu <ryanliu12620@gmail.com>
2025-11-21 10:19:50 -0500

    third commit

--- Day1/Day1Part1.py
+++ Day1/Day1Part1.py
@@ -1,11 +1,19 @@
-import re
+from contextlib import nullcontext
 
-def part1(inputs):
+
+def part1(values):
     total = 0
-    for value in inputs:
-        num1 = re.search(r"d+", value)
-        print(num1)
-    print(inputs)
+    for value in values:
+        updated_value = replace_all_alphabetical(value)
+        if len(updated_value) > 0:
+            first_num = updated_value[0]
+            second_num = updated_value[-1]
+            final_num = int("" + first_num + "" + second_num)
+            total += final_num
+    return total
+
+def part2(values):
+    print('part2')
 
 def open_file(filename):
     with open(filename, "r") as file_input:
@@ -13,5 +21,17 @@ def open_file(filename):
         list_of_values = input.split("\n")
         return list_of_values
 
-file_input = open_file("Day1Input")
-part1(file_input)
+def replace_all_alphabetical(string):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for letter in alphabet:
+        string = string.replace(letter, "")
+    return string
+
+def replace_word_to_num(string):
+    numbers = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    for number in numbers:
+        string = string.replace(number, "")
+    return string
+
+inputs = open_file("Day1Input")
+print(part1(inputs))
